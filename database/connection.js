38c7947110f6ea49b5967f4dc1d5b8206e5199e2
sebasmontoyasm <sebasmontoyasm@gmail.com>
2023-04-13 14:52:21 -0500

    connection with mongo db

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connection = async () => {
     try {
-        await mongoose.connect("mongodb+srv://crezgo:ScDrdF2Ex1m3fqNz@clustercrezgo.rqfyyap.mongodb.net/crezgoDB?retryWrites=true&w=majority");
+        await mongoose.connect("mongodb+srv://sebasmontoyasm:ARLKmkfoL5oeb1UP@crezgodb.o8osfmn.mongodb.net/test");
         console.log('Database connected crezgo DB');
     } catch (error) {
         console.log('No se puede conectar a la base de datos');
